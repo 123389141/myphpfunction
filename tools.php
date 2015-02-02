e67641761aafb44b94be283ae351b8e2dcbdb01e
王志鹏 <123389141@qq.com>
2015-02-02 11:27:14 +0800

    tijiao

---
+++ tools.php
@@ -0,0 +1,48 @@
+<?php
+class tools {
+	//文件下载
+	function download($filedir){  //$filedir为路径+文件名
+		if(is_file($filedir)) {
+			header("Content-Type: application/force-download");
+			header("Content-Disposition: attachment; filename=".basename($filedir));
+			readfile($filedir);
+			exit;
+		}else{
+			echo "{$filedir}文件不存在！";
+			exit;
+		}
+	}
+	//服务器端get请求
+	function requestget($id){
+		$ch = curl_init();
+		$timeout = 5;
+		$url="http://aos.corp.elong.com/apus_web/DeployInstance/GetVersions?nodeId=nodeId&moduleName=moduleName&allowThreeVersion=false&allowFourVersion=true";
+		curl_setopt ($ch, CURLOPT_URL,$url);
+		curl_setopt ($ch, CURLOPT_RETURNTRANSFER, 1);
+		curl_setopt ($ch, CURLOPT_CONNECTTIMEOUT, $timeout);
+		$file_contents = curl_exec($ch);
+		curl_close($ch);
+	}
+	//服务器端发post请求
+	function requestpost($nodeId,$moduleName,$metaId,$i){
+		$ch = curl_init();
+		$timeout = 5;
+		$url="http://aos.corp.elong.com/apus_web/DeployInstance/Run?metaId=$metaId";
+		$data = array (
+				'nodeId' => $nodeId,
+				'version' => $maxver,
+				'deployType'=>'module'
+		);
+		curl_setopt ( $ch, CURLOPT_URL, $url );
+		curl_setopt ( $ch, CURLOPT_POST, 1 );
+		curl_setopt ( $ch, CURLOPT_HEADER, 0 );
+		curl_setopt ( $ch, CURLOPT_RETURNTRANSFER, 1 );
+		curl_setopt ( $ch, CURLOPT_POSTFIELDS, $data );
+		$file_contents = curl_exec($ch);
+		$aaaa =  (Array)json_decode($file_contents);
+		curl_close($ch);
+	}
+	
+}
+
+?>
